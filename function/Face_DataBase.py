548511c232263c9210b58dc004628a48d82cc849
zhant22 <zhant22@mcmaster.com>
2023-06-26 16:14:57 -0400

    putting options for both relative and absolute pah

--- function/Face_DataBase.py
+++ function/Face_DataBase.py
@@ -1,8 +1,9 @@
 import face_recognition
 
 # Load a sample picture and learn how to recognize it.s
-#for some reason the file path has to be absolute path
-DYLAN_image = face_recognition.load_image_file("C:/Users/tzhang/Desktop/personal/fianl_proj_face_recog_secure_system/family_face/DYLAN.jpg")
+
+#DYLAN_image = face_recognition.load_image_file("C:/Users/tzhang/Desktop/personal/fianl_proj_face_recog_secure_system/family_face/DYLAN.jpg")  #for some reason the file path has to be absolute path
+DYLAN_image = face_recognition.load_image_file("family_face\DYLAN.jpg")
 DYLAN_face_encoding = face_recognition.face_encodings(DYLAN_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
