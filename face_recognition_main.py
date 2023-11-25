39e90b3488ffc9474b85cb47bb523dbe83f1e93b
zhant22 <zhant22@mcmaster.com>
2023-11-25 01:04:58 -0500

    editting the summary

--- face_recognition_main.py
+++ face_recognition_main.py
@@ -13,18 +13,16 @@ import function.face_depth_measure as face_depth_measure
 import function.control_hardware as control_hardware
 
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
+# This is a demo of running face recognition on live video from the webcam. it includes some basic performance tweaks 
+# to make things run a lot faster:
+
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
 #   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # This code imports several libraries and defines variables that will be used in facial recognition, patrol mode, 
 # and face-depth measurements. It then creates a loop that runs continuously, taking video frames from a webcam and 
 # processing them.
+
 # In the loop, the code measures the distance between the camera and the face to determine if it is close enough 
 # for facial recognition. It then enters patrol mode if the distance is between a certain range, using Haar cascades 
 # to detect faces and bodies in the video frame. If a face or body is detected, it starts recording video until the 
