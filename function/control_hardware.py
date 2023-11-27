e5b065177c88b3cb3552196e5790203fbf24abe9
zhant22 <zhant22@mcmaster.com>
2023-11-26 23:31:57 -0500

    adding logic for finger print reader

--- function/control_hardware.py
+++ function/control_hardware.py
@@ -61,11 +61,38 @@ def rotate_motor(open_or_close_the_lock, port='com5', baudrate=115200):
             print("arduino not connected. Cannot send command.")
             error_message_printed = True
 
-if __name__ == '__main__':
+def read_data_from_arduino(port='com5', baudrate=115200): 
+    arduinoData = None
+    error_message_printed = False
 
-    rotate_motor('open')
-    turn_on_LED('G')
-    turn_on_LED('OFF')
+    try:
+        arduinoData = serial.Serial(port, baudrate)
+    except serial.SerialException as e:
+        if not error_message_printed:
+            print("Breadboard not connected. Cannot send command, check port and baudrate")
+            error_message_printed = True
+
+    while True:
+        if (arduinoData.in_waiting()>0):
+            myData = arduinoData.read()
+            print(myData)
+            if myData == 'Found a print match!':
+                return True
+
+if __name__ == '__main__':
+    flag = None
+    while True:
+        flag = read_data_from_arduino()
+        if flag:
+            rotate_motor('open')
+            turn_on_LED('G')
+            turn_on_LED('G')
+            turn_on_LED('G')
+            turn_on_LED('R')
+            rotate_motor('close')
+            turn_on_LED('OFF')
+
+        flag = None
 
 
 
