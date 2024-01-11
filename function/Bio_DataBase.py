f6a9ed44e4f4af777726d0171e5dd05813784ff1
zhant22 <zhant22@mcmaster.ca>
2024-01-11 01:08:34 -0500

    adding jackson back as the second example

--- function/Bio_DataBase.py
+++ function/Bio_DataBase.py
@@ -4,13 +4,18 @@ import face_recognition
 DYLAN_image = face_recognition.load_image_file("family_face\DYLAN.jpg")
 DYLAN_face_encoding = face_recognition.face_encodings(DYLAN_image)[0]
 
+JACKSON_image = face_recognition.load_image_file("family_face\Jackson.jpg")
+JACKSON_face_encoding = face_recognition.face_encodings(JACKSON_image)[0]
+
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    DYLAN_face_encoding
+    DYLAN_face_encoding,
+    JACKSON_face_encoding
     
 ]
 known_face_names = [  #make sure to capitalize only the first letter and lower case for the rest of the name
-    "Dylan"
+    "Dylan",
+    "Jackson"
     
 ]
 
@@ -20,4 +25,5 @@ known_friend_names = [  #make sure to capitalize only the first letter and lower
 ]
 
 #below are code added using the graphic user interface.
+
 
