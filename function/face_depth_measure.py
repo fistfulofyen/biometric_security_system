7986e3237e4db1af12fdc739a0bc68932958679e
zhant22 <zhant22@mcmaster.ca>
2024-01-12 01:20:59 -0500

    UPDATE: 1.making the instruction of the calibrate more clear(Global parameter and step by step instructions);  2.creating separate function for the main script to call

--- function/face_depth_measure.py
+++ function/face_depth_measure.py
@@ -3,21 +3,19 @@ print(cv2.__version__)
 import cvzone
 from cvzone.FaceMeshModule import FaceMeshDetector
 
-video_capture = cv2.VideoCapture(0)
+#--------------------------------------------------------------------------------------
+# NOTE: the Global parameter you need to calibrate, follow the step in "main" at bottom
+#--------------------------------------------------------------------------------------
+
+FOCAL_LENGTH_OF_YOUR_PC = 600
 
 
 #-------------------------------------------------------
 # this is the source code for the face_depth measurement
 #-------------------------------------------------------
+video_capture = cv2.VideoCapture(0)
 
-
-def get_distance(calibration_mode =1):
-
-    #NOTE:for every new camera or laptop with webcam that you use, you need to calibrate the "FOCAL_LENGTH_OF_YOUR_PC" parameter,
-    # since every camera has different internal parameter of their only. in order to do that:
-    # first: set the camera around 50 cm away from your face.
-    # second: set the calibration_mode to 0 to start the measurement process. 
-    # finally: change the "FOCAL_LENGTH_OF_YOUR_PC" parameter to the reading printed out. 
+def calibrating_camera(calibration_mode,focal_length_of_your_pc_input):
 
     #calibration_mode = 1  # NOTE: setting this to 0 to initiate the focal length of your own pc and then modify FOCAL_LENGTH_OF_YOUR_PC
     detector = FaceMeshDetector(maxFaces=1)
@@ -45,9 +43,8 @@ def get_distance(calibration_mode =1):
                 test_out_focal_length_for_below = distance_between_head_and_camera*width_of_two_eyes_in_pixels/Width_of_two_eyes_in_cm
                 print(test_out_focal_length_for_below) #NOTE: modify the value below in function
             elif calibration_mode ==1:
-                # finding depth 
-                FOCAL_LENGTH_OF_YOUR_PC = 540 
-                distance_between_head_and_camera = (Width_of_two_eyes_in_cm*FOCAL_LENGTH_OF_YOUR_PC)/width_of_two_eyes_in_pixels
+                # finding distance
+                distance_between_head_and_camera = (Width_of_two_eyes_in_cm*focal_length_of_your_pc_input)/width_of_two_eyes_in_pixels
                 print(distance_between_head_and_camera)
 
         if calibration_mode ==0:
@@ -55,7 +52,38 @@ def get_distance(calibration_mode =1):
             if cv2.waitKey(1) == ord('q'):
                 break
 
+def get_distance(video_capture, detector,FOCAL_LENGTH_OF_YOUR_PC=FOCAL_LENGTH_OF_YOUR_PC):
+    success, img = video_capture.read()
+    img, faces = detector.findFaceMesh(img, draw=False)
+
+    distance_between_head_and_camera = 0  # Initialize the variable
+
+    if faces:
+        face = faces[0]
+        pointLeft = face[145]
+        pointRight = face[374]
+
+        cv2.line(img, pointLeft, pointRight, (0, 200, 0), 3)
+        cv2.circle(img, pointLeft, 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, pointRight, 5, (255, 0, 255), cv2.FILLED)
+
+        width_of_two_eyes_in_pixels, _ = detector.findDistance(pointLeft, pointRight)
+        Width_of_two_eyes_in_cm = 6.3
+        distance_between_head_and_camera = (Width_of_two_eyes_in_cm * FOCAL_LENGTH_OF_YOUR_PC) / width_of_two_eyes_in_pixels
+
+
+    return distance_between_head_and_camera
 
 
 if __name__ == '__main__':
-    get_distance(calibration_mode =1)
+    
+    # NOTE: for every new camera or laptop with webcam that you use, you need to calibrate the "FOCAL_LENGTH_OF_YOUR_PC" parameter,
+    # since every camera has different internal parameter of their only. in order to do that:
+    
+    # 1. place your head around 50 cm away from your webcam.
+    # 2. set the calibration_mode to 0 to start the measurement process. 
+    # 3. change the "FOCAL_LENGTH_OF_YOUR_PC" parameter to the reading printed out. 
+    # 4. set the calibration_mode to 1 to check if the distance measuring makes sense
+
+
+    calibrating_camera(calibration_mode =0, focal_length_of_your_pc_input = FOCAL_LENGTH_OF_YOUR_PC)
